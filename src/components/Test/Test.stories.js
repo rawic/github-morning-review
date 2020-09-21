99719f952b233a2a8c532d4c7ef00166937ac100
Rafał Wichowski <rawic.me@gmail.com>
2020-09-21 13:07:15 +0200

    Storybook init, add test component

---
+++ src/components/Test/Test.stories.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Test from './Test';
+
+export default {
+  title: 'Test',
+  component: Test,
+};
+
+const Template = (args) => <Test {...args} />;
+
+export const Primary = Template.bind({});
+
+Template.args = {};
